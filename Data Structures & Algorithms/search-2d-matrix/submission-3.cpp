f31aa724d595b5f803d2c11380873d6ee1b00aa3
Dev Pandey <113662149+creed-06@users.noreply.github.com>
2026-06-04 16:58:36 +0530

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int binary(vector<int>arr, int find){
+        int l=0,r=arr.size()-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(arr[mid]<find) l=mid+1;
+            else if(arr[mid]>find) r=mid-1;
+            else return true;
+        }
+    return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++){
+            if(binary(matrix[i],target)) return true;
+        }
+    return false;
+    }
+};
